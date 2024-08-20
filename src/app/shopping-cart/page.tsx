0668b9c2b7256263aa7a7872ff4507be3882801f
botirovdev <botirovdev7@gmail.com>
2024-08-20 23:43:42 +0500

    fixed bugs

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -6,16 +6,19 @@ import CustomImage from "../components/image";
 import { useEffect, useState } from "react";
 
 const ShoppingCart = () => {
-  const [total, setTotal] = useState<number>()
-  const [products, setProducts] = useState<ProductType[]>(
-    JSON.parse(localStorage.getItem("carts") as string) || []
-  )
+  const [total, setTotal] = useState<number>();
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const savedProducts = JSON.parse(localStorage.getItem("carts") as string) || [];
+    setProducts(savedProducts);
+  }, []);
 
   const removeProduct = (id: number) => {
-    const updatedCart = products.filter((product) => product.id != id)
-    localStorage.setItem("carts", JSON.stringify(updatedCart))
-    setProducts(updatedCart)
-  }
+    const updatedCart = products.filter((product) => product.id !== id);
+    localStorage.setItem("carts", JSON.stringify(updatedCart));
+    setProducts(updatedCart);
+  };
 
   const handleIncrement = (id: number) => {
     const updatedCart = products.map(product => {
@@ -23,64 +26,59 @@ const ShoppingCart = () => {
         return {
           ...product,
           quantity: product.quantity + 1
-        }
+        };
       }
-
       return product;
     });
 
-    localStorage.setItem("carts", JSON.stringify(updatedCart))
-    setProducts(updatedCart)
-  }
+    localStorage.setItem("carts", JSON.stringify(updatedCart));
+    setProducts(updatedCart);
+  };
 
   const handleDecrement = (id: number) => {
     const existProduct = products.find(product => product.id === id);
     if (existProduct?.quantity === 1) {
-      removeProduct(existProduct.id)
+      removeProduct(existProduct.id);
     } else {
-
       const updatedCart = products.map(product => {
         if (product.id === id) {
           return {
             ...product,
             quantity: product.quantity - 1
-          }
+          };
         }
-
         return product;
       });
 
-      localStorage.setItem("carts", JSON.stringify(updatedCart))
-      setProducts(updatedCart)
+      localStorage.setItem("carts", JSON.stringify(updatedCart));
+      setProducts(updatedCart);
     }
-  }
+  };
 
   useEffect(() => {
     const total = products.reduce((acc, item) => {
-      return acc + item.price * item.quantity
-    }, 0)
-    setTotal(total)
-  }, [products])
+      return acc + item.price * item.quantity;
+    }, 0);
+    setTotal(total);
+  }, [products]);
 
   return (
-
     <>
-    {
-      products.length ? (
+      {products.length ? (
         <div className="h-screen bg-gray-100 pt-20">
           <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
           <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
             <div className="rounded-lg md:w-2/3">
               {products.map(product => (
-                <div key={product.id} className="justify-between mb-6 rounded-lg bg-white p-6  shadow-md sm:flex sm:justify-start">
+                <div key={product.id} className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
                   <div className="relative w-52">
                     <CustomImage product={product} fill />
                   </div>
-                  <div className="sm:ml-4 sm:flex sm:w-full  sm:justify-between">
+                  <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                     <div className="mt-5 sm:mt-0">
                       <h2 className="text-lg font-bold text-gray-900 line-clamp-1">{product?.title}</h2>
                       <p className="mt-1 text-xs text-gray-700 line-clamp-2">{product?.description}</p>
-    
+
                       <div className="flex items-center text-sm my-4">
                         <p>{product?.rating?.rate}</p>
                         {product?.rating?.rate && (
@@ -93,7 +91,6 @@ const ShoppingCart = () => {
                             See all {product.rating.count} reviews
                           </p>
                         )}
-    
                       </div>
                     </div>
                     <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
@@ -105,16 +102,13 @@ const ShoppingCart = () => {
                       <div className="flex items-center space-x-4">
                         <p className="text-sm">{product?.price ? (product.price * product.quantity).toLocaleString("en-US", { style: "currency", currency: "USD" }) : 'N/A'}
                         </p>
-                        <svg onClick={() => removeProduct(product.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        <svg onClick={() => removeProduct(product.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                       </div>
-    
                     </div>
-    
                   </div>
                 </div>
-    
               ))}
             </div>
             <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
@@ -125,7 +119,6 @@ const ShoppingCart = () => {
                     ? total.toLocaleString("en-US", { style: "currency", currency: "USD" })
                     : "N/A"}
                 </p>
-    
               </div>
               <div className="flex justify-between">
                 <p className="text-gray-700">Shipping</p>
@@ -137,8 +130,9 @@ const ShoppingCart = () => {
                 <div className="">
                   <p className="mb-1 text-lg font-bold">
                     {typeof total === 'number'
-                    ? total.toLocaleString("en-US", { style: "currency", currency: "USD" }) + 10
-                    : "N/A"}</p>
+                      ? (total + 10).toLocaleString("en-US", { style: "currency", currency: "USD" })
+                      : "N/A"}
+                  </p>
                   <p className="text-sm text-gray-700">including VAT</p>
                 </div>
               </div>
@@ -146,13 +140,11 @@ const ShoppingCart = () => {
             </div>
           </div>
         </div>
-
       ) : (
-          <h1 className="mt-28 text-center">Your backet is empty</h1>
-      )
-    }
+        <h1 className="mt-28 text-center">Your basket is empty</h1>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
